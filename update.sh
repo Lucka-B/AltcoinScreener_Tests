8320097eed389a33c799f4c74eab125f72ab2963
Lucka-B <lbrettschneiderova@gmail.com>
2020-09-26 13:52:20 +0200

    finished update

--- update.sh
+++ update.sh
@@ -1,16 +1,14 @@
 # clone repository
 git clone git@gitlab.com:martin2201/binance-screener.git
 cd binance-screener
-# clean repository
-sudo git clean -fxd
-git reset --hard HEAD
-# update repository
-git pull
 # install packages
 npm i
 # build app and server
 npm run gui:build
 npm run server:build
 # copy build and server files to build folder
-# no copy happens because of problem with an app
+cp _bin/ ../build/_bin/ -R
+cp _public/ ../build/_public/ -R
 # remove repository
+cd ..
+rm -rf binance-screener
